d9887cccbba7f76e32ce85ff5b1aaeb13134e521
Sandhya S <Sandhs@adobe.com>
2026-04-10 11:25:32 +0530

    final changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,14 +15,15 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'import/extensions': ['error', { js: 'always' }],
+    'linebreak-style': ['error', 'unix'],
+    'no-param-reassign': [2, { props: false }],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: ['**/*.test.js', '**/*.spec.js', 'playwright.config.js'],
     }],
+
     'xwalk/max-cells': ['error', {
-      '*': 4, // default limit for all models
+      '*': 4,
       form: 17,
       wizard: 12,
       'form-button': 7,
@@ -47,8 +48,13 @@ module.exports = {
       rating: 18,
       password: 20,
       tnc: 12,
-      range: 19
+      range: 19,
+
+      // ✅ Your fix
+      'my-custom-card': 20,
     }],
-    'xwalk/no-orphan-collapsible-fields': 'off', // Disable until enhancement is done for Forms properties
+
+    // keep this (you accidentally removed earlier)
+    'xwalk/no-orphan-collapsible-fields': 'off',
   },
 };
